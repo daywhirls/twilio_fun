0fb667c10980acc2c236593a7b3fd49aa19f5e93
bonetail <worleyd@purdue.edu>
2018-07-24 21:57:29 -0400

    created initial sendSMS() feature with static msg

---
+++ send_receive_sms.py
@@ -0,0 +1,16 @@
+import os
+from twilio.rest import Client
+
+# Twilio credentials
+TWILIO_SID = os.environ["TWILIO_SID"]
+TWILIO_AUTH = os.environ["TWILIO_AUTH"]
+
+my_number = os.environ["MY_NUMBER"]
+twilio_number = os.environ["TWILIO_NUMBER"]
+
+client = Client(TWILIO_SID, TWILIO_AUTH)
+client.messages.create(
+    to=my_number,
+    from_=twilio_number,
+    body="Hello, world! This is my first ever Twilio App!"
+)
